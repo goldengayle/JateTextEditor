17223efae53cd9d8ffde01d25200d2e58cfa3b10
goldie <codegayle@gmail.com>
2023-06-11 10:05:10 -0400

    added screenshot. Functionality works

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -28,7 +28,7 @@ export const putDb = async (content) => {
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.log('Get from the database');
-  const jateDb = await jateDb('jate', 1);
+  const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
   const request = store.get(1);
